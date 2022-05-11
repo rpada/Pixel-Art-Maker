df6ff4b92b8ed3c6e83817dd48940b91c07b9bd0
rpada <rpada@hawaii.edu>
2022-05-10 14:16:26 -1000

    comments

--- designs.js
+++ designs.js
@@ -1,28 +1,29 @@
-// Select color input
-// Select size input
-    const $colorPicker = document.getElementById("colorPicker");
-    const $sizePicker = document.getElementById("sizePicker");
-    const $pixelCanvas = document.getElementById("pixelCanvas");
-    const $inputWidth = document.getElementById("inputWidth");
-    const $inputHeight = document.getElementById("inputHeight");
+// Global variables
+    const $colorPicker = document.getElementById("colorPicker"); // Select color input
+    const $sizePicker = document.getElementById("sizePicker"); // Select size input
+    const $pixelCanvas = document.getElementById("pixelCanvas"); // get canvas
+    const $inputWidth = document.getElementById("inputWidth"); // get width
+    const $inputHeight = document.getElementById("inputHeight"); // get height
   
+    // add event listener to the size picker
     $sizePicker.addEventListener('submit', function() {
       event.preventDefault();
-      let width = inputWidth.value
-      let height = inputHeight.value
+      let width = inputWidth.value // get width value
+      let height = inputHeight.value // get height value
       // When size is submitted by the user, call makeGrid()
       makeGrid(width, height);
     })
   
+    // function to create the grid
     function makeGrid(width, height) {
       $pixelCanvas.innerHTML = '';
-      for (let i = 0; i < width; i++) {
-        let row = $pixelCanvas.insertRow(i);
-          for (let j= 0; j < height; j++ ) {
-            let cell = row.insertCell(j);
+      for (let i = 0; i < height; i++) { // iterate up until the height's value
+        let row = $pixelCanvas.insertRow(i); // add a row into the grid
+          for (let j= 0; j < width; j++ ) { // iterate up until the width's value
+            let cell = row.insertCell(j); // add a cell into the row
             cell.addEventListener("click", function(event) {
-              let color = $colorPicker.value
-              cell.style.backgroundColor = color;})
+              let color = $colorPicker.value // get the value of the color 
+              cell.style.backgroundColor = color;}) // assign the color into the cell
         }
       }
     }
